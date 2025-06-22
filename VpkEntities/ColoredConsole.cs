aa8181713be88acb49e9ecd543069d453d7cb9d1
Kyle <kyle@kxnrl.com>
2025-06-22 20:07:18 +0800

    initial commit

---
+++ VpkEntities/ColoredConsole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace VpkEntities;
+
+public static class ColoredConsole
+{
+    private static readonly Lock Lock = new ();
+
+    public static void Print(ConsoleColor color, string buffer, params object[] args)
+    {
+        lock (Lock)
+        {
+            var c = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(buffer, args);
+            Console.ForegroundColor = c;
+        }
+    }
+}
---
+++ VpkEntities/ConsoleAction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace VpkEntities;
+
+public static class ConsoleAction
+{
+    private static TextWriter DefaultWriter = TextWriter.Null;
+
+    public static void StartContext()
+    {
+        DefaultWriter = Console.Out;
+    }
+
+    public static void EndContext()
+    {
+        ColoredConsole.Print(ConsoleColor.Blue, "\n\nPress any key to continue...");
+        Console.ReadKey(true);
+    }
+
+    public static void DisableOutput()
+    {
+        DefaultWriter = Console.Out;
+        Console.SetOut(TextWriter.Null);
+    }
+
+    public static void EnableOutput()
+    {
+        Console.SetOut(DefaultWriter);
+    }
+}
